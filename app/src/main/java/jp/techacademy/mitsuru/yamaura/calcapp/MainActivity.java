e4eea897c53ac0e48467711b4bae663712fe96d3
Mitsuru\mitsuru2 <komayou326@gmail.com>
2018-10-15 18:26:44 +0900

    数字以外の入力を不可、0除算の制限、例外処理の追加

--- app/src/main/java/jp/techacademy/mitsuru/yamaura/calcapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/mitsuru/yamaura/calcapp/MainActivity.java
@@ -41,13 +41,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public void onClick(View v){
+    public void onClick(View v) {
 
-        Intent intent = new Intent(this,SecondActivity.class);
+        Intent intent = new Intent(this, SecondActivity.class);
 
         //  文字列を取得する
-        String s1 =mEditText1.getText().toString();
-        String s2 =mEditText2.getText().toString();
+        String s1 = mEditText1.getText().toString();
+        String s2 = mEditText2.getText().toString();
 
         //  ここで数字以外の文字が入力されたらcatchする
         try {
@@ -55,36 +55,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             x = Float.parseFloat(s1);
             y = Float.parseFloat(s2);
 
-        } catch (NumberFormatException i){
+        } catch (NumberFormatException i) {
             //  NumberFormatExceptionが出たらここで捕まえる
             //  iには例外の情報が入っている
-            Snackbar.make(view, "残念。数字を入力してくださいね", Snackbar.LENGTH_LONG).show();
+            Snackbar.make(v, "残念。数字を入力してくださいね", Snackbar.LENGTH_LONG).show();
         }
 
         //  押したボタンに合わせて条件分岐してTextViewに表示する
-        if(v.getId() == R.id.button1){
+        if (v.getId() == R.id.button1) {
 
             z = x + y;
-            intent.putExtra("VALUE1",z);
+            intent.putExtra("VALUE1", z);
             startActivity(intent);
 
-        }else if(v.getId() == R.id.button2){
+        } else if (v.getId() == R.id.button2) {
 
-            z = x-y;
-            intent.putExtra("VALUE1",z);
+            z = x - y;
+            intent.putExtra("VALUE1", z);
             startActivity(intent);
 
-        }else if(v.getId() == R.id.button3) {
+        } else if (v.getId() == R.id.button3) {
 
             z = x * y;
-            intent.putExtra("VALUE1",z);
+            intent.putExtra("VALUE1", z);
             startActivity(intent);
 
-        }else if(v.getId() == R.id.button4) {
+        } else if (v.getId() == R.id.button4) {
 
             //  0の除算ができるのを回避する
-            if (Y == 0) {
-                Snackbar.make(view, "数字②には0以外を入力してね", Snackbar.LENGTH_LONG).show();
+            if (y == 0) {
+                Snackbar.make(v, "数字②には0以外を入力してね", Snackbar.LENGTH_LONG).show();
             } else {
                 z = x / y;
                 intent.putExtra("VALUE1", z);
