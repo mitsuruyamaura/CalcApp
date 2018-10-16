e3763014ed4a0475848ac2425746e903c04e7f33
Mitsuru\mitsuru2 <komayou326@gmail.com>
2018-10-16 11:06:56 +0900

    EditText未入力時にSnackBar表示で弾く処理の追加

--- app/src/main/java/jp/techacademy/mitsuru/yamaura/calcapp/MainActivity.java
+++ app/src/main/java/jp/techacademy/mitsuru/yamaura/calcapp/MainActivity.java
@@ -2,6 +2,7 @@ package jp.techacademy.mitsuru.yamaura.calcapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.content.Intent;
 import android.widget.Button;
@@ -27,13 +28,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button1.setOnClickListener(this);
 
         Button button2 = (Button) findViewById(R.id.button2);
-        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
 
         Button button3 = (Button) findViewById(R.id.button3);
-        button1.setOnClickListener(this);
+        button3.setOnClickListener(this);
 
         Button button4 = (Button) findViewById(R.id.button4);
-        button1.setOnClickListener(this);
+        button4.setOnClickListener(this);
 
         mEditText1 = (EditText) findViewById(R.id.editText1);
         mEditText2 = (EditText) findViewById(R.id.editText2);
@@ -50,45 +51,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String s2 = mEditText2.getText().toString();
 
         //  ここで数字以外の文字が入力されたらcatchする
-        try {
+        //try {
+            //  float型にキャストする
+            //x = Float.parseFloat(s1);
+            //y = Float.parseFloat(s2);
+
+        //} catch (NumberFormatException i) {
+            //  NumberFormatExceptionが出たらここで捕まえる
+            //  iには例外の情報が入っている
+            //Snackbar.make(v, "残念。数字を入力してくださいね", Snackbar.LENGTH_INDEFINITE).show();
+            //return;
+        //}
+
+        //  EditTextに文字の入力がない状態でボタンを操作した場合
+        if(TextUtils.isEmpty(s1) || TextUtils.isEmpty(s2)) {
+
+            Snackbar.make(v, "残念。数字を入力してくださいね", Snackbar.LENGTH_INDEFINITE).show();
+            return;
+
+        }else {
+
             //  float型にキャストする
             x = Float.parseFloat(s1);
             y = Float.parseFloat(s2);
 
-        } catch (NumberFormatException i) {
-            //  NumberFormatExceptionが出たらここで捕まえる
-            //  iには例外の情報が入っている
-            Snackbar.make(v, "残念。数字を入力してくださいね", Snackbar.LENGTH_LONG).show();
-        }
+            //  押したボタンに合わせて条件分岐してTextViewに表示する
+            if (v.getId() == R.id.button1) {
 
-        //  押したボタンに合わせて条件分岐してTextViewに表示する
-        if (v.getId() == R.id.button1) {
-
-            z = x + y;
-            intent.putExtra("VALUE1", z);
-            startActivity(intent);
-
-        } else if (v.getId() == R.id.button2) {
-
-            z = x - y;
-            intent.putExtra("VALUE1", z);
-            startActivity(intent);
-
-        } else if (v.getId() == R.id.button3) {
-
-            z = x * y;
-            intent.putExtra("VALUE1", z);
-            startActivity(intent);
-
-        } else if (v.getId() == R.id.button4) {
-
-            //  0の除算ができるのを回避する
-            if (y == 0) {
-                Snackbar.make(v, "数字②には0以外を入力してね", Snackbar.LENGTH_LONG).show();
-            } else {
-                z = x / y;
+                z = x + y;
                 intent.putExtra("VALUE1", z);
                 startActivity(intent);
+
+            } else if (v.getId() == R.id.button2) {
+
+                z = x - y;
+                intent.putExtra("VALUE1", z);
+                startActivity(intent);
+
+            } else if (v.getId() == R.id.button3) {
+
+                z = x * y;
+                intent.putExtra("VALUE1", z);
+                startActivity(intent);
+
+            } else if (v.getId() == R.id.button4) {
+
+                //  0の除算ができるのを回避する
+                if (y == 0) {
+                    Snackbar.make(v, "数字②には0以外を入力してね", Snackbar.LENGTH_INDEFINITE).show();
+                } else {
+                    z = x / y;
+                    intent.putExtra("VALUE1", z);
+                    startActivity(intent);
+                }
             }
         }
     }
